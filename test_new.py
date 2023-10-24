88cc4e211072260c00d11e4c3adafbb8bb9c4e13
yugeshbuchipalle <69415092+yugeshbuchipalle@users.noreply.github.com>
2023-10-24 19:30:06 +0530

    Update test_new.py

--- test_new.py
+++ test_new.py
@@ -1,1 +1,8 @@
-
+def test_add_positive_numbers():
+        print("yugesh")  # Assert that add(2, 3) is equal to 5
+def test_add_negative_numbers():
+        print("yugesh")
+def test_add_mixed_numbers():
+        print("yugesh")
+def test_new():
+        print("yugesh")
